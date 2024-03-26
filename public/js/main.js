cd68c0b890e29b8d8576eb3e85410628ad124668
Diego DOminguez Z <software.fe@grupoccima.com>
2024-03-25 23:46:01 -0600

    cambios en el simulador de inversion

--- public/js/main.js
+++ public/js/main.js
@@ -1,85 +1,109 @@
-var series = [
-    {
-        data: [100000, 100000, 100000, 100000, 100000],
+const calculateData = (investment) => {
+  const terreno160 = {
+    valor_inicial: {
+      '1 año': 639936,
+      '2 año': 639936,
+      '3 año': 639936
+    },
+    plusvalia: {
+      '1 año': 63993.6,
+      '2 año': 127987.20,
+      '3 año': 191980.8
+    }
+  };
+
+  const terreno200 = {
+    valor_inicial: {
+      '1 año': 799920,
+      '2 año': 799920,
+      '3 año': 799920
+    },
+    plusvalia: {
+      '1 año': 79992,
+      '2 año': 159984,
+      '3 año': 239976
+    }
+  };
+
+  const terreno128 = {
+    valor_inicial: {
+      '1 año': 614338.56,
+      '2 año': 614338.56
+    },
+    plusvalia: {
+      '1 año': 61433.89,
+      '2 año': 122867.71
+    }
+  };
+
+  let data = {};
+  switch (investment) {
+    case 3:
+      data = terreno160;
+      break;
+    case 4:
+      data = terreno200;
+      break;
+    case 5:
+      data = terreno128;
+      break;
+    default:
+      data = terreno160;
+  }
+
+  return {
+    valor_inicial: Object.values(data.valor_inicial),
+    plusvalia: Object.values(data.plusvalia)
+  };
+};
+
+const getOptionChart = (data) => {
+  return {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    xAxis: {
+      type: 'category',
+      data: ['1 año', '2 años', '3 años']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: data.valor_inicial,
         type: 'bar',
         stack: 'a',
-        name: 'a',
+        name: 'Inversión inicial',
         color: ['#D9D9D9']
       },
       {
-        data: [10000, 20000, 30000, 40000, 50000],
+        data: data.plusvalia,
         type: 'bar',
         stack: 'a',
-        name: 'b',
-        color: ['#002856']
-      },
-      {
-        data: [20000, 30000, 40000, 50000, 60000],
-        type: 'bar',
-        stack: 'a',
-        name: 'c',
-        color: ['#3D7CC9']
-      },
-  ];
-  const stackInfo = {};
-for (let i = 0; i < series[0].data.length; ++i) {
-  for (let j = 0; j < series.length; ++j) {
-    const stackName = series[j].stack;
-    if (!stackName) {
-      continue;
-    }
-    if (!stackInfo[stackName]) {
-      stackInfo[stackName] = {
-        stackStart: [],
-        stackEnd: []
-      };
-    }
-    const info = stackInfo[stackName];
-    const data = series[j].data[i];
-    if (data && data !== '-') {
-      if (info.stackStart[i] == null) {
-        info.stackStart[i] = j;
+        name: 'Plusvalía',
+        color: ['#568CCA']
       }
-      info.stackEnd[i] = j;
-    }
-  }
-}
-for (let i = 0; i < series.length; ++i) {
-  const data = series[i].data;
-  const info = stackInfo[series[i].stack];
-  for (let j = 0; j < series[i].data.length; ++j) {
-    // const isStart = info.stackStart[j] === i;
-    const isEnd = info.stackEnd[j] === i;
-    const topBorder = isEnd ? 20 : 0;
-    const bottomBorder = 0;
-    data[j] = {
-      value: data[j],
-      itemStyle: {
-        borderRadius: [topBorder, topBorder, bottomBorder, bottomBorder]
-      }
-    };
-  }
-}
-
-const getOptionChart=()=>{
-    return {
-        xAxis: {
-          type: 'category',
-          data: ['2 años', '4 años', '6 años', '8 años', '10 años'],
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: series
-      };
-};
-const initCharts=()=>{
-    const chart=echarts.init(document.getElementById('chart'));
-
-
-    chart.setOption(getOptionChart());
+    ]
+  };
 };
 
-window.addEventListener("load", () =>{
-    initCharts();
+const initCharts = () => {
+  const chart = echarts.init(document.getElementById('chart'));
+  const initialInvestment = 3; // Inversión inicial predeterminada (terreno 160 m2)
+  const data = calculateData(initialInvestment);
+  chart.setOption(getOptionChart(data));
+
+  document.getElementById('investment-select-2').addEventListener('change', function () {
+    const selectedInvestment = parseInt(this.value);
+    const newData = calculateData(selectedInvestment);
+    chart.setOption(getOptionChart(newData));
+});
+};
+
+window.addEventListener("load", () => {
+  initCharts();
 });
